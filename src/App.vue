36e8b1be0f0ad1568ff01581d85dd25ae6102121
gemdeng <tanc77914@gmail.com>
2023-12-20 17:52:12 +0800

    init autofit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <script setup lang="ts">
-import Scene from '@/views/Scene.vue'
+import Scene from "@/views/Scene.vue";
+import { onMounted } from "vue";
+
+import autofit from "autofit.js";
+onMounted(() => {
+  autofit.init({
+    dh: 4573,
+    dw: 2179,
+     el: "#app-main",
+     resize:true
+  })
+}),
 </script>
 
 <template>
-<!--  <Scene/>-->
-  <router-view></router-view>
+  <!--  <Scene/>-->
+  <div id="app-main">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style scoped>
+#app-main {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  top: 0;
+  left: 0;
+  z-index: 2;
+}
 :global(#app) {
-  width: 7938px;
-  height: 3780px;
-  //width:100vw;
-  //height: 100vh;
+  width: 100vw;
+  height: 100vh;
   background-color: #999;
   pointer-events: none;
 }
